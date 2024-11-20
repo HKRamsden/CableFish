508f70fef7fd426cb9b58e4176360819eddfc68b
Hannah Ramsden <114274438+HKRamsden@users.noreply.github.com>
2024-11-20 16:45:33 -0500

    Update with choice inclusion & Part 1 Basic

--- cableFish.py
+++ cableFish.py
@@ -12,6 +12,7 @@ import time
 
 # Specialized imports
 from tkinter import *
+from scapy.all import *
 import scapy.all as scapy
 from rich.console import Console
 from rich.style import Style
@@ -21,12 +22,63 @@ console = Console()
 # Styles
 darkCyan = Style(color="#0094ab")  # dark cyan color
 
-# Welcome screen
-console.print("[#0094ab]          Welcome to CableFish!          [/#0094ab]", style="bold underline")
-with console.status("     Loading Application", spinner="shark"):
-    time.sleep(5.0)
-console.print("        :fish: Successfully Loaded :fish:")
+# Functions
 
-#request = scapy.ARP() 
-#print(request.show())
+# Function to sniff packets 
+def sniff_packets():
+    console.print("Enter Total Number of Packets to Sniff: ")
+    totalNum = int(input())
+    
+    packets = sniff(count=totalNum)
+    
+    with console.status("      Sniffing packets", spinner="shark"):
+        time.sleep(3.0)
+    console.print(":fish: Finished Sniffing :fish:")
+    packets.summary()
+    
+    
+    
+#function to scan port
+def scan_port():
+    console.print("stank ass ho")
 
+def mainMenu():
+    while True:
+        console.print("")
+
+        console.print("                   :fish: :desktop_computer:  [#114159]Select Options:[/#114159] :desktop_computer:  :fish:", style="bold")
+        console.print(":fish: [#1dd3ef]Option 1: Sniff Packets [/#1dd3ef]:fish:")
+        console.print(":desktop_computer:  [#0094ab]Option 2: Port Scans [/#0094ab]:desktop_computer:")
+        console.print(":fish: [#1dd3ef]Option 3: [/#1dd3ef]:fish:")
+        console.print(":desktop_computer:  [#0094ab]Option 4: [/#0094ab]:desktop_computer:")
+        console.print(":fish: [#1dd3ef]Option 5: [/#1dd3ef]:fish:")
+        console.print(":desktop_computer:  [#0094ab]Option 6: Exit [/#0094ab]:desktop_computer:")
+
+        console.print("")
+        console.print("[#0094ab]                        Enter Your Choice:                       [/#0094ab]", style="bold underline")
+
+        # General Option Input 
+        optionChoice = int(input())
+
+        # Match case to handle user input
+        # console.print("Your Choice: ", optionChoice)
+        match optionChoice:
+            case 1:
+                sniff_packets()
+            case 2:
+                 scan_port()
+            case 6:
+                console.print("exit")
+                break
+            case _:
+                console.print("Try Again")
+                
+# Initial Menu
+console.print("\n[#0094ab]                      Welcome to CableFish!                      [/#0094ab]", style="bold underline")
+with console.status("                             Loading Application", spinner="shark"):
+    time.sleep(3.0)
+console.print("                    :fish: Successfully Loaded :fish:")                
+                
+# Loop to run menu           
+if __name__ == "__main__":
+    mainMenu()
